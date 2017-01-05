b19dbe9668a5d2be2a20125f56315755095c378b
littlebusters <little.busters.reply@gmail.com>
2017-01-06 01:20:28 +0900

    init

---
+++ Switch Text Spacing.sketchplugin/Contents/Resources/l18n.js
@@ -0,0 +1,35 @@
+var l18n = ['en', 'ja-JP'];
+l18n['en'] = {
+	'messageText': 'Switch Text Spacing',
+	'infoText': 'Depending on the font, it may not be switched.',
+	'options': [
+		'Proportional Widths',
+		'Full Width',
+		'Half Width',
+		'Third Width',
+		'Quarter Width',
+		'Alternate Proportional Widths',
+		'Alternate Half Width',
+		'No Change (Default)',
+		'No Kerning'
+	],
+	'switched': 'Switched to "',
+	'nonSelected': 'You have not selection!'
+}
+l18n['ja-JP'] = {
+	'messageText': 'Switch Text Spacing',
+	'infoText': '使っているフォントによっては、設定が適用されない場合があります。',
+	'options': [
+		'プロポーショナル幅',
+		'全角',
+		'半角',
+		'三分幅',
+		'四分幅',
+		'代替プロポーショナル幅',
+		'代替半角',
+		'変更なし（デフォルト）',
+		'カーニングなし'
+	],
+	'switched': '」に変更しました！',
+	'nonSelected': 'レイヤーを選択してください。'
+}
